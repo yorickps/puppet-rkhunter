6a287830264fa980f3611eac21b4f20e378bba3b
Alexander Fisher <alex@linfratech.co.uk>
2019-03-19 12:17:13 +0000

    Run `--propupd` after config file is written

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -14,7 +14,10 @@ class rkhunter::packages(
 
   # Run rkhunter --propupd after installation of package
   exec { '/usr/bin/rkhunter --propupd':
-    unless    => '/usr/bin/test -f /var/lib/rkhunter/db/rkhunter_prop_list.dat && /usr/bin/test -f /var/lib/rkhunter/db/rkhunter.dat',
-    subscribe => Package['rkhunter'],
+    unless  => '/usr/bin/test -f /var/lib/rkhunter/db/rkhunter_prop_list.dat && /usr/bin/test -f /var/lib/rkhunter/db/rkhunter.dat',
+    require => [
+      Package['rkhunter'],
+      File['/etc/rkhunter.conf'],
+    ],
   }
 }
