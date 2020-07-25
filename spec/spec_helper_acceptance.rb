b37579e25b0dd6636d4a4bf66f9aa1a64a95f558
Tim Meusel <tim@bastelfreak.de>
2020-07-25 14:22:29 +0200

    modulesync 3.0.0

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,22 +1,6 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+# This file is completely managed via modulesync
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+configure_beaker
 
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  c.before :suite do
-    install_module
-    install_module_dependencies
-    hosts.each do |host|
-      if fact('os.family') == 'RedHat'
-        # EPEL provides rkhunter
-        install_package(host, 'epel-release')
-      end
-    end
-  end
-end
+Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
